efa91ca3b34443ee1b61b27a13f1b7a65e427346
rts-6905 <U2418040@rts-365.dk>
2023-09-01 13:42:30 +0200

    commit done

--- app.js
+++ app.js
@@ -5,32 +5,104 @@ var myMusic = [
     {
         title: "Stairway to Heaven",
         published:1971,
-        genre: [" Hård rock", "Progressiv rock"],
+        genres: [" Hård rock", "Progressiv rock"],
         artist: ["Led Zeppelin"],
     },
     {
         title: "Purple Haze",
         published:1967,
-        genre: [" Klassisk rock"],
+        genres: [" Klassisk rock"],
         artist: ["Jimi Hendrix"],
     },
     {
         title: "Bohemian Rhapsody",
         published:1975,
-        genre: ["Hård rock", " Indie/alternativt", "Pop"],
+        genres: ["Hård rock", " Indie/alternativt", "Pop"],
         artist: ["Queen"],
     },
     {
         title: "Free Bird",
         published:1973,
-        genre: [" Southern rock", "Hård rock"],
+        genres: [" Southern rock", "Hård rock"],
         artist: ["Lynyrd Skynyrd"],
     },
     {
         title: "Fortunate Son",
         published:1969,
-        genre: ["Rock"],
+        genres: ["Rock"],
         artist: ["Creedence Clearwater Revival"],
+    },
+    {
+        title: "Purple Rain",
+        published:1984,
+        genres: ["pop"],
+        artist: ["Prince"],
+    },
+    {
+        title: "rorkes drift",
+        published:2016,
+        genres: ["power metal"],
+        artist: ["Sabaton"],
+    },
+    {
+        title: "paint it black",
+        published:2012,
+        genres: ["Rock"],
+        artist: ["The Rolling Stones"],
+    },
+    {
+        title: "Sweet Home Alabama",
+        published:1974,
+        genres: ["Country rock"],
+        artist: ["Lynyrd Skynyrd"],
+    },
+    {
+        title: "Take On Me",
+        published:1985,
+        genres: [" Synthpop", "New wave"],
+        artist: [" a-ha"],
+    },
+    {
+        title: "Paper Planes",
+        published:2007,
+        genres: ["Indie/alternativt"],
+        artist: ["M.I.A."],
+    },
+    {
+        title: "I Want It That Way",
+        published:1999,
+        genres: ["Pop"],
+        artist: ["Backstreet Boys"],
+    },
+    {
+        title: "Happy",
+        published:2014,
+        genres: ["Soul", "Pop"],
+        artist: ["Pharrell Williams"],
+    },
+    {
+        title: "everybody",
+        published:1997,
+        genres: ["Pop"],
+        artist: ["Backstreet Boys"],
+    },
+    {
+        title: "Till I Collapse",
+        published:2002,
+        genres: [" Hiphop/rap"],
+        artist: ["Eminem"],
+    },
+    {
+        title: "California Love",
+        published:1995,
+        genres: ["hip hop gangsta rap "],
+        artist: ["Tupac Shakur", " Dr. Dre"],
+    },
+    {
+        title: "Straight Outta Compton",
+        published:1988,
+        genres: ["West Coast hip hop gangsta rap hardcore hip hop"],
+        artist: ["N.W.A."],
     },
 ]
 
@@ -44,14 +116,14 @@ function submitHandler(event) {
     const RESULTS = myMusic.filter(function(element){
         return searchTitle(event.target.search.value, element.title)
         || compare(element.published, event.target.search.value)
-        || findInArray(element.genre, event.target.search.value)
+        || findInArray(element.genres, event.target.search.value)
         || findInArray(element.artist, event.target.search.value)
     })
 
     console.log(RESULTS)
     const UL = document.getElementsByClassName("musicResults")[0]
 
-    RESULTS.forEach(function(result){
+    RESULTS.forEach(function(result) {
         const LI = document.createElement("li")
         LI.innerHTML = `
         <h2 class="music__Title">${result.title}</h2>
@@ -59,7 +131,7 @@ function submitHandler(event) {
         <h3>genre</h3>
         <ul class="musicGenres"></ul>
         <h3>kunstner</h3>
-        <ul class="musicArtist"></ul>
+        <ul class="musicArtists"></ul>
         `
         const GENRES = LI.querySelector(".musicGenres")
         const ARTISTS = LI.querySelector(".musicArtists")
@@ -85,37 +157,3 @@ function searchTitle(keyword, title) {
         return item.toLowerCase().includes(neddle.toLowerCase())
     })
 }
-
-const BANDS = [
-    {
-        name: "Led Zeppelin",
-        age: 1970,
-        genre: ["Hård rock"],
-    },
-    {
-        name: "Jimi Hendrix",
-        age: 1970,
-        genre: ["Klassisk rock"],
-    },
-    {
-        name: "Queen",
-        age: 1970,
-        genre: ["Hård rock", "Indie/alternativt", "Pop" ],
-    },
-    {
-        name: "Lynyrd Skynyrd",
-        age: 1964,
-        genre: ["Southern rock", "Hård rock"],
-    },
-    {
-        name: "Creedence Clearwater Revival",
-        age: 1959,
-        genre: ["Rock"],
-    },
-]
- 
-BANDS.forEach(function(result){
-    document.body.innerHTML += `
-    <h2>${result.name} is a ${result.age} 
-    band with the genre  ${result.genre}</h2>`
-})
